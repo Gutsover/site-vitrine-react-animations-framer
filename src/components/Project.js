9f7b25b7b087f8da2275b10982e60dd54728f883
Gutsover <mylenevandaele@gmail.com>
2021-06-14 12:10:31 +0200

    add infos contact & copy-to-clipboard

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,33 +10,40 @@ const Project = (props) => {
   let top = Math.floor(Math.random() * 200 + 150) + "px";
   let size = "scale(" + (Math.random() + 0.7) + ")";
 
-
   return (
     <div className="project-main">
       <div className="project-content">
         <h1>{project.title}</h1>
         <p>{project.date}</p>
         <ul className="languages">
-           {project.languages.map((item) => {
-               return <li key={item}>{item}</li>
-           })} 
+          {project.languages.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
         </ul>
       </div>
       <div className="img-content">
-           <div className="img-container hover">
-            <span>
-                <h3>{project.title}</h3>
-                <p>{project.infos}</p>
-            </span>
-            <img src={project.img} alt={project.title} />
-           </div>
-           <div className="button-container">
-            <a href={project.link} target="_blank" rel="noopener noreferrer" className="hover">
+        <div className="img-container hover">
+          <span>
+            <h3>{project.title}</h3>
+            <p>{project.infos}</p>
+          </span>
+          <img src={project.img} alt={project.title} />
+        </div>
+        <div className="button-container">
+          <a
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover"
+          >
             <span className="button">voir le site</span>
-            </a>
-           </div>
+          </a>
+        </div>
       </div>
-      <span className="random-circle" style={{left, top, transform: size}}></span>
+      <span
+        className="random-circle"
+        style={{ left, top, transform: size }}
+      ></span>
     </div>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,69 @@
 import React from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import ButtonsBottom from "../components/ButtonsBottom";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
 
 const Contact = () => {
-  return <div>Call me baby</div>;
+  return (
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        {/* contact form */}
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>12 rue React</p>
+              <p>59000 Lille</p>
+            </div>
+          </div>
+
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0612121212" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Numéro de téléphone copié !");
+                  }}
+                >
+                  06 12 12 12 12
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="react@react.fr" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Email copié !");
+                  }}
+                >
+                  react@react.fr
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Mylène V - 2021</p>
+          </div>
+        </div>
+        <ButtonsBottom left={"/projet-4"} />
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
